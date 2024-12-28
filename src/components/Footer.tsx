392c3944be23d287b3d839df42b17af3f8a9c9f6
Yuki Saito <saitoyukisaito@gmail.com>
2024-12-27 21:03:32 -0800

    hide footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,14 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center justify-center pb-32 sm:flex-row-reverse sm:justify-between">
       <Socials />
-      <section className="mt-8 sm:mt-0">
+      {/* <section className="mt-8 sm:mt-0">
         <p className="text-center text-xs text-muted-foreground">
           <span>&copy; {new Date().getFullYear()}</span>{" "}
           <Link className="link" href="/">
             tedawf.com
           </Link>
         </p>
-      </section>
+      </section> */}
     </footer>
   );
 }
